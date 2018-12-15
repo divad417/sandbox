188027bf360feedd0dc873a749e9c83b529c0878
David Brown <divad417@gmail.com>
2018-12-15 13:34:21 -0500

    Change to single method

--- kalman/kalman.py
+++ kalman/kalman.py
@@ -7,45 +7,47 @@ import numpy as np
 class kalman():
 
     # Init assuming 2d problem with no control input or process noise
-    def __init__(self, dt, dx):
+    def __init__(self, t0, dx):
+        self.t = t0
         # Measurement function
         self.H = np.array([[1, 0, 0, 0], [0, 1, 0, 0]])
         # Measurement uncertainty
         self.R = np.array([[dx, 0], [0, dx]])
-        # State function
+        # Initialize state
+        self.x = np.zeros([4,1])
+        # Initialize variance
+        self.P = 1000 * np.identity(4)
+
+    def step(self, t, z):
+        dt = t - self.t
+        self.t = t
+        # State function with new dt
         self.F = np.identity(4)
         self.F[0,2] = dt
         self.F[1,3] = dt
 
-    def update(self, x, P, z):
-        y = z - np.dot(self.H, x)
-        S = np.dot(self.H, np.dot(P, self.H.transpose())) + self.R
-        K = np.dot(np.dot(P, self.H.transpose()), np.linalg.inv(S))
-        x = x + np.dot(K, y)
-        P = np.dot((np.identity(4) - np.dot(K, self.H)), P)
-        return(x, P)
+        # Update
+        y = z - np.dot(self.H, self.x)
+        S = np.dot(self.H, np.dot(self.P, self.H.transpose())) + self.R
+        K = np.dot(np.dot(self.P, self.H.transpose()), np.linalg.inv(S))
+        self.x = self.x + np.dot(K, y)
+        self.P = np.dot((np.identity(4) - np.dot(K, self.H)), self.P)
+        print(self.x)
 
-    def predict(self, x, P):
-        x = np.dot(self.F, x)
-        P = np.dot(self.F, np.dot(P, self.F.transpose()))
-        return(x, P)
+        # Predict
+        self.x = np.dot(self.F, self.x)
+        self.P = np.dot(self.F, np.dot(self.P, self.F.transpose()))
 
-dt = 0.1    # sec timestep
-dx = 0.1      # m uncertainty in position measurement
-dxi = 1000  # m initial uncertainty in position
+dx = 0.1    # m uncertainty in position measurement
+k = kalman(0, dx)
 
+data = [[0, 0, 0], [0.1, 1, 1], [0.2, 2, 2], [0.3, 3, 3], [0.4, 4, 4], [0.5, 5, 5]]
 
-k = kalman(dt, dx)
-x = np.zeros([4,1])
-P = 1000 * np.identity(4)
-
-data = [[0, 0], [1,1], [2,2], [3,3], [4,4]]
+x = []
 
 for i in data:
-    z = np.array([i])
+    z = np.array([i[1:2]])
     z = z.transpose()
-    x, P = k.update(x, P, z)
-    x, P = k.predict(x, P)
-    print(x)
-    print(P)
+    t = i[0]
+    k.step(t, z)
 
