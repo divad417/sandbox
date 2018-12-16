a2e7da4ed2d6585d481db46207c0936536ddf1d1
David Brown <divad417@gmail.com>
2018-12-15 20:26:40 -0500

    Add plots

--- kalman/kalman.py
+++ kalman/kalman.py
@@ -3,6 +3,9 @@
 # Python class to implement a Kalman Filter
 
 import numpy as np 
+import matplotlib.pyplot as plt
+from random import random
+from math import *
 
 class kalman():
 
@@ -32,22 +35,56 @@ class kalman():
         K = np.dot(np.dot(self.P, self.H.transpose()), np.linalg.inv(S))
         self.x = self.x + np.dot(K, y)
         self.P = np.dot((np.identity(4) - np.dot(K, self.H)), self.P)
-        print(self.x)
+        x = self.x
+        P = self.P
 
         # Predict
         self.x = np.dot(self.F, self.x)
         self.P = np.dot(self.F, np.dot(self.P, self.F.transpose()))
+        return(x, P)
 
-dx = 0.1    # m uncertainty in position measurement
-k = kalman(0, dx)
+def line(n, dx):
+    x0 = 0
+    y0 = 0
+    theta = pi/4
+    v = 10
+    dt = 0.1
 
-data = [[0, 0, 0], [0.1, 1, 1], [0.2, 2, 2], [0.3, 3, 3], [0.4, 4, 4], [0.5, 5, 5]]
+    t = np.linspace(0, 10, 101)
+    pos = np.empty([n, 2])
+    for i in range(n):
+        x_new = x0 + t[i] * v * cos(theta)
+        y_new = y0 + t[i] * v * sin(theta)
+        pos[i,:] = [x_new, y_new]
+    
+    return(t, pos)
+        
 
-x = []
 
-for i in data:
-    z = np.array([i[1:2]])
-    z = z.transpose()
-    t = i[0]
-    k.step(t, z)
+def main():
 
+    # Initialize
+    dx = 0.1    # m uncertainty in position measurement
+    k = kalman(0, dx)
+
+    n = 51
+    t, truth = line(n, dx)
+    meas = np.empty([n, 2])
+    filt = np.empty([n, 4])
+
+    for i in range(n):
+        z = np.array(truth[i,:], ndmin=2)
+        z = z.transpose()
+        x, P = k.step(t[i], z)
+        filt[i,:] = np.transpose(x)
+
+
+    plt.subplot(2,1,1)
+    plt.plot(truth,'+')
+    plt.plot(filt,'--')
+
+    plt.subplot(2,1,2)
+    plt.plot()
+    plt.show()
+
+main()
