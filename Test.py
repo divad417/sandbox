53b665f4dbe4800536a4f5c0a41fd1a863d30688
David Brown <dbrown@optimusride.com>
2019-01-23 14:57:11 -0500

    System flags

--- Test.py
+++ Test.py
@@ -1,1 +1,12 @@
-print "Hello World"
+#!/usr/bin/env python3
+
+import pandas as pd
+
+import pandas as pd
+import numpy as np
+
+a = pd.DataFrame(np.eye(5),columns=['a','b','c','d','e'])
+
+
+
+print(a)
